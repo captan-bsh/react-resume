0d977b33005ffcad2c9d79e15aa4ab9bcfcf6c72
BshCport <captan.bsh@gmail.com>
2019-04-01 16:19:17 +0430

    Updates

---
+++ precache-manifest.5ca289b561bae5d10084ab357a5cbe96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6208f3df1d1368c3fc44964cbf063c50",
+    "url": "/react-resume/static/media/first-blur.6208f3df.jpg"
+  },
+  {
+    "revision": "9ebccc51354bc6ce9a39",
+    "url": "/react-resume/static/js/runtime~main.bbd03709.js"
+  },
+  {
+    "revision": "019e521511625c95016d",
+    "url": "/react-resume/static/js/main.ffaf7d11.chunk.js"
+  },
+  {
+    "revision": "16a2151a976da2026875",
+    "url": "/react-resume/static/js/2.d346c2d9.chunk.js"
+  },
+  {
+    "revision": "019e521511625c95016d",
+    "url": "/react-resume/static/css/main.686a9615.chunk.css"
+  },
+  {
+    "revision": "16a2151a976da2026875",
+    "url": "/react-resume/static/css/2.f105939c.chunk.css"
+  },
+  {
+    "revision": "211a3f9863217060ffe88f97b7013b96",
+    "url": "/react-resume/index.html"
+  }
+];
